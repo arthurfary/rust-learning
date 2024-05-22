a329c1775b0e8e7a68473b973cbbd6f0b8735f97
arthurfary <arthurfary22@gmail.com>
2024-05-22 00:47:02 -0300

    simple web server

--- rust/rust-web-app/src/main.rs
+++ rust/rust-web-app/src/main.rs
@@ -2,10 +2,14 @@
 
 use nickel::Nickel;
 use std::fs;
-use std::io;
 
-fn read_file(file_path: &str) -> io::Result<String> {
-    fs::read_to_string(file_path); 
+// readfile takes a string literal and returns a 
+// Result type with the ok being a String type
+fn read_file(file_path: &str) -> String {
+    match fs::read_to_string(file_path){
+        Ok(content) => content,
+        Err(_) => "Error reading file.".to_string(),
+    }
 }
 
 fn main() {
@@ -13,12 +17,9 @@ fn main() {
 
     server.utilize(router! {
         get "**" => |_req, _res| {
-             match read_file("index.html") {
-                Ok(content) => content,
-                Err(_) => "Error reading file.".to_string(),
-            }
+            read_file("index.html")
         }
     });
 
-    server.listen("127.0.0.1:6767");
+    server.listen("127.0.0.1:6767").expect("Error starting the server.");
 }
